daa58a4edffd26f87372ab6a1bb0d0395af88237
Rodrigo Manhães <rmanhaes@gmail.com>
2010-08-22 03:29:12 -0300

    added formtastic and haml

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,8 @@ Rails::Initializer.run do |config|
 
   config.gem 'feedzirra'
   config.gem 'inherited_resources', :version => '1.0.6'
+  config.gem 'formtastic'
+  config.gem 'haml'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
